615d816d068b4d0f5e8df601930b5f160bf7eda1
Tim Murray <timmurray@google.com>
2015-02-05 10:29:43 -0800

    Rebase Eigen to 3.2.4.
    Change-Id: Ia5161a916545b0a7903825b57faac438086320f3

--- Android.mk
+++ Android.mk
@@ -11,6 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# This empty file is here solely for the purpose of optimizing the Android build
-# Please keep it there, and empty, thanks.
